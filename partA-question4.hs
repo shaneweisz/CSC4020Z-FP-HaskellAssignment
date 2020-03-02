6b8f147e666f0f81c17f83f121427caaefa998a5
Shane Weisz <shaneweisz@gmail.com>
2020-03-02 13:09:48 +0200

    Working perms

--- partA-question4.hs
+++ partA-question4.hs
@@ -6,8 +6,9 @@ safetail xs = if null xs then [] else tail xs
 -- (2)
 
 safetail' :: [a] -> [a]
-safetail' xs | not (null xs) = tail xs
-             | otherwise = []
+safetail' xs
+    | null xs = []
+    | otherwise = tail xs
 
 -- (3)
 
--- partB-question3.hs
+++ partB-question3.hs
@@ -3,3 +3,7 @@ delete v [] = []
 delete v (x:xs)
     | x /= v = [x] ++ delete v xs
     | x == v = xs
+
+perms :: [Int] -> [[Int]]
+perms [] = [[]]
+perms xs = [i:j | i <- xs, j <- perms (delete i xs)]
